dca01dd457b19a607843abccbd53d8f635817711
SimonVenier <70879289+SimonVenier@users.noreply.github.com>
2021-05-09 23:16:16 +0200

    Lets goo =D

---
+++ app/Models/Post.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+
+class Post
+{
+
+    public static function all(){
+        $files = File::files(resource_path("posts/"));
+
+        return array_map(function ($file){
+
+            return $file -> getContents();
+
+        }, $files);
+    }
+
+
+    public static function find($slug){
+
+        base_path();
+
+        if(!file_exists($path = resource_path("posts/{$slug}.html"))){
+
+            throw new ModelNotFoundException();
+
+            //ddd('file does not exist');
+            //return redirect('/');
+            //abord(404);
+        }
+
+        return cache()->remember("posts.{$slug}", 1200, function () use ($path) {
+            return file_get_contents($path);
+        });
+
+    }
+
+}
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+
+<body>
+    <?php foreach ($posts as $post) : ?>
+
+    <article>
+
+        <?= $post; ?>
+
+    </article>
+
+    <?php endforeach; ?>
+</body>
